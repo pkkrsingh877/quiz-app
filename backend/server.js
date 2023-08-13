ad20f8df2e09834c47b4723483aa6f2a44720bb9
Prabhat Kumar <pkkrsingh877@gmail.com>
2023-08-13 14:23:54 +0530

    Backend development start

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+try {
+    mongoose.connect(process.env.MONGO_URI);
+} catch (error) {
+    console.log(error);
+}
 
 app.get('/', (req, res) => {
     res.send('Server is running');
