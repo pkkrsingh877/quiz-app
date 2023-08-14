1d9e20f6f0812cb808302f2df335decb03322be5
Prabhat Kumar <pkkrsingh877@gmail.com>
2023-08-14 15:21:34 +0530

    questioncreate page up and running, added link to reach it too

--- frontend/src/components/QuestionCreate.js
+++ frontend/src/components/QuestionCreate.js
@@ -1,26 +1,54 @@
+import React, { useState } from 'react';
+
 const QuestionCreate = () => {
-    return ( 
+    const [text, setText] = useState('');
+    const [options, setOptions] = useState(['', '']);
+    const [correctOption, setCorrectOption] = useState('');
+    const [category, setCategory] = useState('');
+
+    const handleOptionChange = (index, value) => {
+        const newOptions = [...options];
+        newOptions[index] = value;
+        setOptions(newOptions);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Here you can handle form submission and send data to the backend
+        // Make sure to use the state variables (text, options, correctOption, category)
+    };
+
+    return (
         <div className="flex-container">
-            {/* Inside a question there exist
-                    -A Text Property to store question
-                    -A Options Property to store object id of options
-                    -A Correct Option Property to store single object id of correct option
-                    -A Category id to which the question will be grouped as
-             */}
             <h2>Create A Question</h2>
             <form className="create" onSubmit={handleSubmit}>
-                <label htmlFor="">Question: </label>
-                <input type="text" />
-                <label htmlFor="">Options: </label>
-                <input type="text" />
-                <label htmlFor="">Correct Option: </label>
-                <input type="text" />
-                <label htmlFor="">Category: </label>
-                <input type="text" />
-                <button>Create</button>
+                <label>Question: </label>
+                <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+                <label>Options: </label>
+                {options.map((option, index) => (
+                    <input
+                        key={index}
+                        type="text"
+                        value={option}
+                        onChange={(e) => handleOptionChange(index, e.target.value)}
+                    />
+                ))}
+                <label>Correct Option: </label>
+                <input
+                    type="text"
+                    value={correctOption}
+                    onChange={(e) => setCorrectOption(e.target.value)}
+                />
+                <label>Category: </label>
+                <input
+                    type="text"
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                />
+                <button type="submit">Create</button>
             </form>
         </div>
-     );
-}
- 
+    );
+};
+
 export default QuestionCreate;
